d1e39fc55ba806f845f51b150d29c07c1ab35e15
Shintamol Thomas <shintamoljobins@gmail.com>
2026-06-20 19:04:26 +0530

    initializatiuon

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let english = Number(prompt("Enter marks for English: "));
+let malayalam = Number(prompt("Enter marks for Malayalam:"));
+let computer =  Number(prompt("Enter marks for Computer:"));
+let mathematics=Number(prompt("Enter marks for Mathematics:"));
+let biology= Number(prompt("Enter marks for Biology:"));
+//total marks
+let totalMarks = english + malayalam + computer + mathematics + biology;
+//average marks
+let averageMarks = totalMarks/5;
+
+
