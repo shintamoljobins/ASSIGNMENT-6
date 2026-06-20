5de301d5c038c39dc9f98416fd4a1187ebb25ca9
Shintamol Thomas <shintamoljobins@gmail.com>
2026-06-20 19:05:32 +0530

    final

--- scripts/script.js
+++ scripts/script.js
@@ -8,4 +8,24 @@ let totalMarks = english + malayalam + computer + mathematics + biology;
 //average marks
 let averageMarks = totalMarks/5;
 
+let grade
+if (averageMarks >= 90){
+    grade = "A+";
+}else if (averageMarks >=80 && averageMarks <=89){
+    grade = "A";
+}else if (averageMarks >=70 && averageMarks <=79){
+    grade = "B";
+}else if (averageMarks >=60 && averageMarks <=69){
+    grade = "C";
+}else if (averageMarks >=50 && averageMarks <=59){
+    grade = "D";
+}else{
+    grade = "F";
+}
+ 
+//display
+alert("GRADE REPORT \n " +
+"Total Marks: " + totalMarks + "\n"+
+"Average Marks: " + averageMarks.toFixed(2) + "\n" +
+"Assigned Grade: " + grade );
 
